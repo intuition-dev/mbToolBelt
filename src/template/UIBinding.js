8889c4d84e8971f4379f01dce1c1e695baa8374d
Vic <vic@eml.cc>
2019-12-21 18:48:08 -0500

    d

--- src/template/UIBinding.js
+++ src/template/UIBinding.js
@@ -12,3 +12,28 @@ depp.require(['DOM'], function() {
 function pushState() {
 
 }
+
+
+function testE1() {
+
+   depp.require('eventBus', function(){
+     console.log('tst:')
+
+     // data before
+     DeventBus.dispatch('dataB4', 'oh hi b4')
+     DeventBus.dispatch('dataB4', 'oh hi b4')
+     DeventBus.addListener('dataB4', function(data) {
+       console.log('b4', data)
+
+     })
+
+     // data after
+     DeventBus.addListener('dataAf', function(data) {
+       console.log('af:', data)
+     })
+     DeventBus.dispatch('dataAf', 'oh hi af')
+     DeventBus.dispatch('dataAf', 'oh hi af')   
+
+   })
+ }//()
+
