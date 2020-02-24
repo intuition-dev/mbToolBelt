91e49abaa895f73583a36d6ce7d31eb0e49b0ba4
Vic <vic@eml.cc>
2020-02-24 00:02:41 -0500

    sync
    sync

--- custel/custel1/custel/c-custel.js
+++ custel/custel1/custel/c-custel.js
@@ -1,10 +1,8 @@
-
-// get the boilerplate:
-import { CompElement } from '/custel/CompElement.js'
-
-class Custel1  extends CompElement {
-  
-   template=`
+import { CompElement } from '/custel/CompElement.js';
+class Custel1 extends CompElement {
+    constructor() {
+        super();
+        this.template = `
    <style>:host {
       all: initial;
       display: block;
@@ -13,37 +11,24 @@ class Custel1  extends CompElement {
    
    <b>I'm a Cust. El</b>
    <slot></slot>
-   `
-
-   state = {}  // could hold state internally, but I use ViewModel externally
-   
-   constructor() {
-      super()
-      console.log('h1')
-      this.setup(this.template)
-
-      this.sr.addEventListener('click', function (e) {
-         console.log(e.composedPath()[0])
-      })//click
-
-      //example of sending message to page
-      depp.require(['eventBus'], function() {
-         DeventBus.dispatch('c-custel-x', { a: 'b', c: 'd' }) 
-      })
-
-   }//cons
-
-   //register properties w/ reflection to attributes, and get pg message or get attribute
-   static get observedAttributes() { return ['bla', 'bla2'] }
-   attributeChangedCallback(aName, oldVal, newVal) { // handler
-      console.log('custel received message', aName, newVal)
-   }//()
-
-   setViewModel(vm) {// methods 
-      console.log('a ViewModel can be set if separation is required, or use EventBus to be loosely coupled')
-   }//()
-}//custel
-
-customElements.define('c-custel', Custel1 )
-
-console.log('loaded')
+   `;
+        this.state = {};
+        console.log('h1');
+        this.setup(this.template);
+        this.sr.addEventListener('click', function (e) {
+            console.log(e.composedPath()[0]);
+        });
+        depp.require(['eventBus'], function () {
+            DeventBus.dispatch('c-custel-x', { a: 'b', c: 'd' });
+        });
+    }
+    static get observedAttributes() { return ['bla', 'bla2']; }
+    attributeChangedCallback(aName, oldVal, newVal) {
+        console.log('custel received message', aName, newVal);
+    }
+    setViewModel(vm) {
+        console.log('a ViewModel can be set if separation is required, or use EventBus to be loosely coupled');
+    }
+}
+customElements.define('c-custel', Custel1);
+console.log('loaded');
---
+++ custel/custel1/custel/c-custel.ts
@@ -0,0 +1,49 @@
+
+// get the boilerplate:
+import { CompElement } from '/custel/CompElement.js'
+
+class Custel1  extends CompElement {
+  
+   template=`
+   <style>:host {
+      all: initial;
+      display: block;
+      contain: content;
+   }</style>
+   
+   <b>I'm a Cust. El</b>
+   <slot></slot>
+   `
+
+   state = {}  // could hold state internally, but I use ViewModel externally
+   
+   constructor() {
+      super()
+      console.log('h1')
+      this.setup(this.template)
+
+      this.sr.addEventListener('click', function (e) {
+         console.log(e.composedPath()[0])
+      })//click
+
+      //example of sending message to page
+      depp.require(['eventBus'], function() {
+         DeventBus.dispatch('c-custel-x', { a: 'b', c: 'd' }) 
+      })
+
+   }//cons
+
+   //register properties w/ reflection to attributes, and get pg message or get attribute
+   static get observedAttributes() { return ['bla', 'bla2'] }
+   attributeChangedCallback(aName, oldVal, newVal) { // handler
+      console.log('custel received message', aName, newVal)
+   }//()
+
+   setViewModel(vm) {// methods 
+      console.log('a ViewModel can be set if separation is required, or use EventBus to be loosely coupled')
+   }//()
+}//custel
+
+customElements.define('c-custel', Custel1 )
+
+console.log('loaded')
