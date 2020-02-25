ba6586ddaf12e835249cd04ab94296f299209b7d
Vic <vic@eml.cc>
2020-02-25 11:51:35 -0500

    d

--- eventFlux/EventFlux.ts
+++ eventFlux/EventFlux.ts
@@ -1,21 +1,22 @@
 
 // https://github.com/theiconic/event-bus
 
-// This is used for FLUX
 
 // added code at end
 // error for Nat: was for in. Should be regular loop. 
 
-var EventBus = function () {
-    this.topics = {};
-  };
+/**
+ * Event bus
+ */
+export class EventFlux  {
+    topics = {}
   
   /**
    * Get a topic in the event bus, if it doesn't exist, it will be created
    * @param {string} identifier 
    * @returns {Topic}
    */
-  EventBus.prototype.getTopic = function (identifier, options) {
+  getTopic(identifier, options) {
     if (!this.topics[identifier]) {
       this.topics[identifier] = new Topic(identifier, options);
     }
@@ -26,33 +27,40 @@ var EventBus = function () {
   /**
    * @returns {Topic[]}
    */
-  EventBus.prototype.getTopics = function () {
+  getTopics() {
     return this.topics;
   };
   
+} // class
+
+class Topic {
+  listeners = {};
+  queue = {};
+
+  identifier
+  options 
+
   /**
    * @param {string} identifier 
    */
-  var Topic = function (identifier, options) {
-    this.identifier = identifier;
-    this.options = options || {};
-    this.listeners = {};
-    this.queue = {};
-  };
+  constructor(identifier, options) {
+    this.identifier = identifier
+    this.options = options || {}
+  }
   
   /**
    * @param {string} binding The event binding to listen to
    * @param {Function} callback The callback to be triggered on events
    * @returns {Listener}
    */
-  Topic.prototype.addListener = function (binding, callback) {
+  addListener(binding, callback) {
     var listener = new Listener(binding, callback, this);
   
     this.listeners[binding] = this.listeners[binding] || [];
     this.listeners[binding].push(listener);
   
     if (this.options.persistent === true) {
-      consume(binding, this.queue, listener);
+      this.consume(binding, this.queue, listener);
     }
   
     return listener;
@@ -61,7 +69,7 @@ var EventBus = function () {
   /**
    * @param {Listener} listener The listener instance to remove
    */
-  Topic.prototype.removeListener = function (listener) {
+  removeListener(listener) {
     var i = 0,
       listeners = this.listeners[listener.event] || [];
   
@@ -77,17 +85,17 @@ var EventBus = function () {
    * @param {string} event The event to trigger
    * @param {any} data The data to broadcast to the listeners
    */
-  Topic.prototype.dispatch = function (event, data) {
+  dispatch(event, data) {
     var binding,
       listeners = [];
   
     for (binding in this.listeners) {
-      if (matchWildCard(event, binding)) {
+      if (this.matchWildCard(event, binding)) {
         listeners = listeners.concat(this.listeners[binding]);
       }
     }
   
-    for (i in listeners) { // Nat fix:
+    for (var i in listeners) { // Nat fix:
         if(listeners[i].callback)
             listeners[i].callback(data);
     }
@@ -98,9 +106,9 @@ var EventBus = function () {
     }
   };
   
-  function consume(binding, queue, listener) {
+  consume(binding, queue, listener) {
     for (var key in queue) {
-      if (matchWildCard(key, binding)) {
+      if (this.matchWildCard(key, binding)) {
         var messages = queue[key];
   
         for (var i in messages) {
@@ -112,27 +120,34 @@ var EventBus = function () {
     }
   }
   
-  function matchWildCard(str, rule) {
+  matchWildCard(str, rule) {
     return new RegExp('^' + rule.split('*').join('.*') + '$').test(str);
   }
-  
+}
+
+class Listener {
+
+  event;
+  callback;
+  topic;
+
   /**
    * @param {string} event The event this listener is attached to
    * @param {Function} callback The actual callable that will be triggered on events
    * @param {Topic} topic A reference to the topic this listener is attached to
    */
-  var Listener = function (event, callback, topic) {
+  constructor(event, callback, topic) {
     this.event = event;
     this.callback = callback;
     this.topic = topic;
   };
   
-  Listener.prototype.unbind = function () {
+  unbind() {
     this.topic.removeListener(this);
-  };
+  }
   
- 
-  
-  // added this:
-  window.eventBus = new EventBus()
-  window.DeventBus = eventBus.getTopic('DEFAULT',{'persistent': true}) // default event bus
+}// class
+
+
+// added this:
+window.DeventBus = new EventFlux().getTopic('DEFAULT',{'persistent': true}) // default event bus
